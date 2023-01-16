b69dbb2672b791e9315132f1c86896c84760015e
Cam-Bloom <cam.w.bloomfield@gmail.com>
2023-01-16 13:38:24 +0000

    Review changes made, removed unnecessary tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,10 +16,6 @@ afterAll(() => {
 // TEST SUITE
 describe("/api/topics", () => {
   describe("GET", () => {
-    test("200: Should recieve a 200 code for a sucessful request", () => {
-      return request(app).get("/api/topics").expect(200);
-    });
-
     test('200: Should respond with a body of topic objects in an array, with "slug" and "description properties"', () => {
       return request(app)
         .get("/api/topics")
@@ -32,28 +28,6 @@ describe("/api/topics", () => {
             expect(topic).toHaveProperty("slug");
             expect(topic).toHaveProperty("description");
           });
-        });
-    });
-
-    test("500: Should respond with a 500 code if DB is not connected or seeded ", () => {
-      return db
-        .query(`DROP TABLE IF EXISTS comments;`)
-        .then(() => {
-          return db.query(`DROP TABLE IF EXISTS articles;`);
-        })
-        .then(() => {
-          return db.query(`DROP TABLE IF EXISTS users;`);
-        })
-        .then(() => {
-          return db.query(`DROP TABLE IF EXISTS topics;`);
-        })
-        .then(() => {
-          request(app)
-            .get("/api/topics")
-            .expect(500)
-            .then(({ body }) => {
-              expect(body.msg).toBe("Internal Server Error");
-            });
         });
     });
   });
